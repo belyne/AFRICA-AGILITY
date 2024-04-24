9aa4977bf436727363d7fedfdae6b9c7b126e75c
belyne <nzabonamarie@gmail.com>
2024-04-24 22:00:57 +0200

    second assignment

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 const port = 3000;
 
@@ -22,6 +26,53 @@ app.patch("/david", (req, res)=> {
 
 app.delete("/david", (req, res)=> {
   res.send("Nice meeting you David"); 
+})
+
+app.post('/api/user', (req, res) => {
+  const body = req.body
+
+  res.json({message: "post successful", "body": body})
+})
+
+app.get('/api/user', (req, res) => {
+
+  res.json({message: "get all users"})
+})
+
+app.get('/api/user/:id', (req, res) => {
+
+  const id = req.params.id
+
+  res.json({message: `Get a user with id ${id}`})
+})
+
+app.post('/api/user/:id', (req, res) => {
+
+  const id = req.params.id
+
+  res.json({message: `Create a user with id ${id}`})
+})
+
+
+app.put('/api/user/:id', (req, res) => {
+
+  const id = req.params.id
+
+  res.json({message: `This user with id ${id} was updated`})
+})
+
+app.patch('/api/user/:id', (req, res) => {
+
+  const id = req.params.id
+
+  res.json({message: `this user with id ${id} was partially updated`})
+})
+
+app.delete('/api/user/:id', (req, res) => {
+
+  const id = req.params.id
+
+  res.json({message: `this user with id ${id} was deleted`})
 })
 
 app.listen(port, () => {
